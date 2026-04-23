0324dfbc4d5cc4e92b5e05d54723ede5c6fcd645
wyatt522 <harrisw522@gmail.com>
2026-04-23 17:22:41 -0400

    generates imu data using oystersim code

--- Phase2/Code/data_gen.py
+++ Phase2/Code/data_gen.py
@@ -3,12 +3,14 @@ import pickle
 import subprocess
 import argparse
 from uav_trajectory import UAVTrajectoryGenerator
+from imu_gen import generate_imu_data
+import numpy as np
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--blender_path", default="/Downloads/blender-5.1.0-linux-x64/blender")
     parser.add_argument("--base_blender_scene", default="Phase2/Blender/test_scene.blend")
-    parser.add_argument("--output_dir", default="Phase2/Output")
+    parser.add_argument("--output_dir", default="Phase2/Output/Sample")
     parser.add_argument("--headless", action="store_true", default=True)
     args = parser.parse_args()
 
@@ -18,7 +20,12 @@ def main():
 
     # 1. Generate Trajectory [cite: 10, 31]
     generator = UAVTrajectoryGenerator()
-    trajectory = generator.generate_circle(duration=5, frequency=10) 
+    trajectory = generator.generate_circle(duration=5, frequency=10)
+
+    # Generate IMU Data
+    imu_data = generate_imu_data(trajectory)
+    with open(args.output_dir + '/imu_data.npy', 'wb') as f:
+        np.save(f, imu_data)
 
     # 2. Save to Pickle [cite: 4]
     pkl_path = os.path.abspath("Phase2/Data/trajectory.pkl")
---
+++ Phase2/Code/imu_gen.py
@@ -0,0 +1,46 @@
+import numpy as np
+from ImuUtils import acc_gen, gyro_gen, vib_from_env, gyro_low_accuracy, accel_low_accuracy 
+
+
+def generate_imu_data(trajectory_data):
+    if len(trajectory_data) < 2:
+        print("ERROR: NO TRAJECTORY DATA")
+        return None
+    else:
+        dt = trajectory_data[1]['time'] - trajectory_data[0]['time']
+        fs = 1.0 / dt
+
+    # 2. Extract Position and Orientation into numpy arrays
+    # Shape: (num_samples, 3)
+    pos = np.array([[s['x'], s['y'], s['z']] for s in trajectory_data])
+    # Assuming angles are in radians
+    ori = np.array([[s['roll'], s['pitch'], s['yaw']] for s in trajectory_data])
+
+    # 3. Calculate ref_w (Angular Velocity)
+    # First derivative of orientation: (rad/s)
+    ref_w = np.gradient(ori, dt, axis=0)
+
+    # 4. Calculate ref_a (Linear Acceleration)
+    # Second derivative of position: (m/s^2)
+    # Note: IMUs measure "proper acceleration," so you may need to add 
+    # gravity (9.81) to the Z-axis depending on your 'acc_gen' requirements.
+    velocity = np.gradient(pos, dt, axis=0)
+    ref_a = np.gradient(velocity, dt, axis=0)
+
+
+    # sets random vibration to accel with RMS for x/y/z axis - 1/2/3 m/s^2, can be zero or changed to other values
+    acc_env = '[0.03 0.001 0.01]-random'
+    acc_vib_def = vib_from_env(acc_env, fs)
+
+    real_acc = acc_gen(fs, ref_a, accel_low_accuracy, acc_vib_def)
+
+
+    # sets sinusoidal vibration to gyro with frequency being 0.5 Hz and amp for x/y/z axis being 6/5/4 deg/s
+    gyro_env = '[6 5 4]d-0.5Hz-sinusoidal'
+    gyro_vib_def = vib_from_env(gyro_env, fs)
+
+    real_gyro = gyro_gen(fs, ref_w, gyro_low_accuracy, gyro_vib_def)
+
+    imu_data = np.concat([real_acc, real_gyro], axis=1)
+
+    return imu_data
