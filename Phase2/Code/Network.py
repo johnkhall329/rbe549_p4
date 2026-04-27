7017664541b7ac7f94fb428033fd42df75f440df
John Hall <johnk.hall329@gmail.com>
2026-04-27 18:22:45 -0400

    store hidden state

--- Phase2/Code/Network.py
+++ Phase2/Code/Network.py
@@ -17,15 +17,12 @@ class DeepVIO(nn.Module):
             hidden_size=1024,#64, 
             num_layers=2,
             batch_first=True)
-        self.rnn
         
         self.rnnIMU = nn.LSTM(
             input_size=6, 
             hidden_size=6,
             num_layers=2,
             batch_first=True)
-        self.rnnIMU
-
         self.feature_extractor = nn.Sequential(
             nn.Conv2d(2, 32, kernel_size=7, stride=2, padding=3), # 480x260
             nn.ReLU(),
@@ -33,23 +30,20 @@ class DeepVIO(nn.Module):
             nn.ReLU(),
             nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1), # 120x65
             nn.ReLU(),
-            nn.AdaptiveAvgPool2d((4, 4)) # Forces the output to 4x4 regardless of input size
+            nn.AdaptiveAvgPool2d((4, 2)) # Forces the output to 4x2 regardless of input size
         )
-        # 128 channels * 4 * 4 = 2048 features
-        self.compression = nn.Linear(2048, 1024)
         
         self.linear1 = nn.Linear(1024, 128)
         self.linear2 = nn.Linear(128, 6)
         #self.linear3 = nn.Linear(128, 6)
-        self.linear1
-        self.linear2
         #self.linear3.cuda()
         
         self.flow_model = raft_small(weights=Raft_Small_Weights.DEFAULT)
-        self.flow_model
 
         for param in self.flow_model.parameters(): # freeze RAFT params
             param.requires_grad = False
+
+        self.hidden_state = None
         
 
     def forward(self, image, imu, xyzQ):
@@ -67,7 +61,6 @@ class DeepVIO(nn.Module):
         # c_out = flow_out.view(batch_size,-1)
         c_out = self.feature_extractor(flow_out)
         c_out = c_out.view(batch_size,-1)
-        c_out = self.compression(c_out)
         #print('c_out', c_out.shape)
         
         ## Input2: Feed IMU records to LSTM
@@ -87,7 +80,8 @@ class DeepVIO(nn.Module):
         cat_out = torch.cat((r_in, imu_out), 2)#1 1 49158
         cat_out = torch.cat((cat_out, xyzQ), 2)#1 1 49165
         
-        r_out, (h_n, h_c) = self.rnn(cat_out)
+        r_out, (h_n, h_c) = self.rnn(cat_out, self.hidden_state)
+        self.hidden_state = (h_n.detach(), h_c.detach())
         l_out1 = self.linear1(r_out[:,-1,:])
         l_out2 = self.linear2(l_out1)
         #l_out3 = self.linear3(l_out2)
@@ -95,10 +89,15 @@ class DeepVIO(nn.Module):
         return l_out2
 
 if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     net = DeepVIO()
+    net = net.to(device)
 
-    img = torch.randn((2,2,3,520,960)) # Width and Weight may change
-    imu = torch.randn((2,10,6))
-    xyzQ = torch.randn((2,1,7))
+    img = torch.randn((2,2,3,520,960), device=device) # Width and Weight may change
+    imu = torch.randn((2,10,6), device=device)
+    xyzQ = torch.randn((2,1,7), device=device)
 
-    net(img, imu, xyzQ)
+    out = net(img, imu, xyzQ)
+    print(out)
+    out = net(img, imu, xyzQ)
+    print(out)
