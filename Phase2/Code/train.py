dcd98beeab584f5cf464b7515cf536dfe3c2612a
John Hall <johnk.hall329@gmail.com>
2026-05-01 20:35:36 -0400

    good for training?

--- Phase2/Code/train.py
+++ Phase2/Code/train.py
@@ -25,7 +25,7 @@ def loss(output_twist, output_pose, gt_twist, gt_pose, global_weight, rot_weight
 
     total_loss = (1-global_weight)*twist_loss + global_weight*global_loss
 
-    return total_loss
+    return total_loss, twist_loss, global_loss
 
 
 def train(args):
@@ -49,10 +49,17 @@ def train(args):
     # batch_first=True is standard for your VINet LSTM training 
     dataloader = DataLoader(dataset, batch_size=args.traj_set, shuffle=True, num_workers=2)
 
+    if not os.path.exists(args.checkpoint_path):
+        os.makedirs(args.checkpoint_path)
+
+    if not os.path.exists(args.log_path):
+        os.makedirs(args.log_path)
+
     # Initialize Model
     model = DeepVIO(model_type=args.model_type)
     model.to(device)
-    abs_traj = None
+
+    writer = SummaryWriter(args.log_path+args.run_name)
 
     optimizer = torch.optim.AdamW(model.parameters(), args.l_rate)
     global_weight_init = 0.01
@@ -62,14 +69,14 @@ def train(args):
 
     scale_x = (final_x - init_x)/(epochs - 1)
 
-    for epoch_i in tqdm(range(epochs)):
+    for epoch_i in tqdm(range(epochs), desc="Epochs"):
         model.train()
 
         # find the global and f2f weights for this epoch
         global_weight = np.exp(-(init_x + (scale_x*epoch_i)))
-        print(global_weight)
+        # print(global_weight)
 
-        for i, (video_paths, imu, gt) in enumerate(dataloader):
+        for traj_set_i, (video_paths, imu, gt) in enumerate(dataloader):
             
             # images shape: [Batch, Seq_Len, C, H, W]
             # imu shape: [Batch, Seq_Len*10, 6]
@@ -79,11 +86,16 @@ def train(args):
 
             imu = imu.to(device)
             gt = gt.to(device)
-            print(f"Batch {i} - Images: {data_transforms(decoders[0][0]).shape}, IMU: {imu.shape}, GT: {gt.shape}")
+            # print(f"Batch {i} - Images: {data_transforms(decoders[0][0]).shape}, IMU: {imu.shape}, GT: {gt.shape}")
 
             start_pos = gt[:,[0]]
             traj_pos = relative_start(start_pos,start_pos)
-            for j in range(decoders[0].metadata.num_frames - 1):
+
+            total_loss = 0
+            total_twist_loss = 0
+            total_global_loss = 0
+
+            for j in tqdm(range(decoders[0].metadata.num_frames - 1), desc="Sequence"):
                 curr_img_pairs = torch.stack([data_transforms(decoders[d][j:j+2]) for d in range(len(decoders))])
                 curr_imu_data = imu[:, j*10:(j+1)*10]
                 gt_data = relative_start(gt[:, j:j+2], start_pos)
@@ -95,13 +107,37 @@ def train(args):
                 # convert se3 to SE3 for loss and loop input ...
                 new_pose = process_output(out_twist, traj_pos)
                 gt_twist = get_twist(gt_data)
-                total_loss = loss(out_twist, new_pose, gt_twist, gt_data[:, [1], :], global_weight)
+                traj_loss, twist_loss, global_loss  = loss(out_twist, new_pose, gt_twist, gt_data[:, [1], :], global_weight)
 
-                total_loss.backwards()
+                total_loss += traj_loss
+                total_twist_loss += twist_loss
+                total_global_loss += global_loss
+
+                traj_loss.backward()
                 optimizer.step()
-                
 
+            writer.add_scalar("Total_loss", total_loss/decoders[0].metadata.num_frames, traj_set_i+(epoch_i*len(dataloader)))
+            writer.add_scalar("Twist_loss", twist_loss/decoders[0].metadata.num_frames, traj_set_i+(epoch_i*len(dataloader)))
+            writer.add_scalar("Pose_loss", global_loss/decoders[0].metadata.num_frames, traj_set_i+(epoch_i*len(dataloader)))
+        
 
+        if epoch_i % args.save_ckpt_epoch == 0 and epoch_i > 0:
+            SaveName = (
+                args.checkpoint_path
+                + args.run_name
+                + str(epoch_i)
+                + ".ckpt"
+            )
+
+            torch.save(
+                {
+                    "epoch": epoch_i,
+                    "model_state_dict": model.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(), 
+                }, 
+                SaveName, 
+            )
+            print("\n" + SaveName + " Model Saved...")
                 
     
 
@@ -112,6 +148,10 @@ if __name__ == '__main__':
     parser.add_argument('--traj_set', type=int, default=3)
     parser.add_argument('--epochs', type=int, default=40)
     parser.add_argument('--l_rate', type=float, default=1e-4)
+    parser.add_argument('--log_path',default="./Phase2/Logs/",help="logs path")
+    parser.add_argument('--run_name', default="test",help="folder to store images")
+    parser.add_argument('--checkpoint_path',default="./Phase2/Checkpoints/",help="checkpoints path")
+    parser.add_argument('--save_ckpt_epoch',default=5,help="num of iteration to save checkpoint")
     args = parser.parse_args()
 
     train(args)
