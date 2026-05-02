83345ce49d61a5027b47615600a3d5cfc95771d3
John Hall <johnk.hall329@gmail.com>
2026-05-02 15:55:37 -0400

    plotting results

--- Phase2/Code/test.py
+++ Phase2/Code/test.py
@@ -7,6 +7,11 @@ from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as transforms
 import numpy as np
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
+
+from scipy.spatial.transform import Rotation as Rot
+
 try:
     from torchcodec.decoders import VideoDecoder
 except:
@@ -124,6 +129,47 @@ def test(args):
         np.savetxt(output_dir+'stamped_traj_estimate.txt', output_poses, header="time x y z qx qy qz qw")
         np.savetxt(output_dir+'stamped_groundtruth.txt', gt_poses, header="time x y z qx qy qz qw")
 
+        fig = plt.figure(figsize=(12, 8))
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(gt_poses[:,1], gt_poses[:,2], gt_poses[:,3], label='Ground Truth Trajectory', color='g', linewidth=2)
+        est_points = output_poses[:,1:4].reshape(-1,1,3)
+        segments = np.concatenate([est_points[:-1], est_points[1:]], axis=1)
+        lc = Line3DCollection(segments, cmap='viridis')
+        lc.set_array(times)
+        lc.set_linewidth(2)
+        lc.set_label('Estimated Trajectory')
+        line = ax.add_collection3d(lc)
+        ax.set_title(model_name+' '+traj_name)
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_zlabel("Z")
+        ax.legend()
+        ax.set_xlim(np.min([gt_poses[:,1],output_poses[:,1]]), np.max([gt_poses[:,1],output_poses[:,1]]))
+        ax.set_ylim(np.min([gt_poses[:,2],output_poses[:,2]]), np.max([gt_poses[:,2],output_poses[:,2]]))
+        ax.set_zlim(np.min([gt_poses[:,3],output_poses[:,3]]), np.max([gt_poses[:,3],output_poses[:,3]]))
+        
+        fig2 = plt.figure(figsize=(12, 8))
+        gt_rpy = np.zeros((gt_poses.shape[0],3))
+        for i, quat in enumerate(gt_poses[:,4:]):
+            rot = Rot.from_quat(quat)
+            gt_rpy[i] = rot.as_euler('xyz', degrees=True)
+        est_rpy = np.zeros((gt_poses.shape[0],3))
+        for i, quat in enumerate(output_poses[:,4:]):
+            rot = Rot.from_quat(quat)
+            est_rpy[i] = rot.as_euler('xyz', degrees=True)
+        plt.plot(times, gt_rpy[:,0], label="GT Roll")
+        plt.plot(times, gt_rpy[:,1], label="GT Pitch")
+        plt.plot(times, gt_rpy[:,2], label="GT Yaw")
+
+        plt.plot(times, est_rpy[:,0], label="Est Roll")
+        plt.plot(times, est_rpy[:,1], label="Est Pitch")
+        plt.plot(times, est_rpy[:,2], label="Est Yaw")
+        plt.xlabel("Time")
+        plt.ylabel("Angle (Deg)")
+        plt.legend()
+
+        plt.show()
+
                 
 
 if __name__ == '__main__':
@@ -134,7 +180,7 @@ if __name__ == '__main__':
     parser.add_argument('--output_path',default="./Phase2/Output/",help="logs path")
     parser.add_argument('--run_name', default="test",help="folder to store images")
     parser.add_argument('--checkpoint_path',default="./Phase2/Checkpoints/",help="checkpoints path")
-    parser.add_argument('--model_name',default="test3010.ckpt",help="checkpoint model name")
+    parser.add_argument('--model_name',default="test3020.ckpt",help="checkpoint model name")
     args = parser.parse_args()
 
     test(args)
